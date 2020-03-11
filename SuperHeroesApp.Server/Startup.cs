9e67e03a193c06c397268b501c61cd4fbbababd8
Kate Zorina <kateryna@zwapgrid.com>
2020-03-11 06:34:40 +0200

    Add superhero components

--- SuperHeroesApp.Server/Startup.cs
+++ SuperHeroesApp.Server/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using SuperHeroesApp.Server.Data;
+using SuperHeroesApp.ComponentClassLib.Services;
 
 namespace SuperHeroesApp.Server
 {
@@ -28,7 +28,7 @@ namespace SuperHeroesApp.Server
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddSingleton<ISuperHeroesService, SuperHeroesService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
